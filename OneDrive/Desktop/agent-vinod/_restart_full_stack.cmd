7e4427a5c05a7d0ffef587a8520cafa541e36f08
Malav Joshih <mujoshi001@gmail.com>
2026-03-22 19:56:54 +0530

    chore: remove debug artifacts

--- OneDrive/Desktop/agent-vinod/_restart_full_stack.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "c:\Users\Lenovo\OneDrive\Desktop\agent-vinod"
-for /f "usebackq tokens=1,* delims==" %%A in (".env") do (
-  if not "%%A"=="" if not "%%A:~0,1"=="#" set "%%A=%%B"
-)
-call scripts\stop-local.cmd
-call scripts\start-cloud-stagehand.cmd
